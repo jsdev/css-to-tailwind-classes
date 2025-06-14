cef9ab6ce9629624aef775c551eac800afde20ef
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2025-06-13 23:59:36 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/css-to-tailwind-classes/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
